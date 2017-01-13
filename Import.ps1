60896d81eec5d0e1be22dca9c18d54d3351cb67d
Ryan Butler <ryan.butler@thinkahead.com>
2017-01-13 13:24:12 -0600

    progress on FTA

--- Import.ps1
+++ Import.ps1
@@ -50,7 +50,7 @@ $paths = $folder -split "\\"|where{$_ -ne ""}
 
 function clean-appobject ($app)
 {
-$tempvar = @{}
+$tempvar = New-Object PSCustomObject
 foreach($t in $app.PSObject.Properties)
     {       
         if(-not ([string]::IsNullOrWhiteSpace($t.Value)))
@@ -101,66 +101,25 @@ function set-UserPerms ($app)
 
 }
 
-function clean-appobject ($app)
-{
-$tempvar = @{}
-foreach($t in $app.PSObject.Properties)
-    {       
-        if(-not ([string]::IsNullOrWhiteSpace($t.Value)))
-        {
-            switch ($t.name)
-            {
-                "AdminFolderName" {$tempvar|Add-Member -MemberType NoteProperty -Name "AdminFolder" -Value $t.Value}
-                "ApplicationGroup" {$tempvar|Add-Member -MemberType NoteProperty -Name "ApplicationGroup" -Value $t.Value}
-                "ApplicationType" {$tempvar|Add-Member -MemberType NoteProperty -Name "ApplicationType" -Value $t.Value}
-                "AssociatedUserNames" {$tempvar|Add-Member -MemberType NoteProperty -Name "AssociatedUserNames" -Value $t.Value}
-                "BrowserName" {$tempvar|Add-Member -MemberType NoteProperty -Name "BrowserName" -Value $t.Value}
-                "ClientFolder" {$tempvar|Add-Member -MemberType NoteProperty -Name "ClientFolder" -Value $t.Value}
-                "CommandLineArguments" {$tempvar|Add-Member -MemberType NoteProperty -Name "CommandLineArguments" -Value $t.Value}
-                "CommandLineExecutable" {$tempvar|Add-Member -MemberType NoteProperty -Name "CommandLineExecutable" -Value $t.Value}
-                "CpuPriorityLevel" {$tempvar|Add-Member -MemberType NoteProperty -Name "CpuPriorityLevel" -Value $t.Value}
-                "Description" {$tempvar|Add-Member -MemberType NoteProperty -Name "Description" -Value $t.Value}
-                "Enabled" {$tempvar|Add-Member -MemberType NoteProperty -Name "Enabled" -Value $t.Value}
-                "MaxPerUserInstances" {$tempvar|Add-Member -MemberType NoteProperty -Name "MaxPerUserInstances" -Value $t.Value}
-                "MaxTotalInstances" {$tempvar|Add-Member -MemberType NoteProperty -Name "MaxTotalInstances" -Value $t.Value}
-                "Name" {$tempvar|Add-Member -MemberType NoteProperty -Name $t.Name -Value $app.BrowserName}
-                "Priority" {$tempvar|Add-Member -MemberType NoteProperty -Name "Priority" -Value $t.Value}
-                "PublishedName" {$tempvar|Add-Member -MemberType NoteProperty -Name "PublishedName" -Value $t.Value}
-                "SecureCmdLineArgumentsEnabled" {$tempvar|Add-Member -MemberType NoteProperty -Name "SecureCmdLineArgumentsEnabled" -Value $t.Value}
-                "ShortcutAddedToDesktop" {$tempvar|Add-Member -MemberType NoteProperty -Name "ShortcutAddedToDesktop" -Value $t.Value}
-                "ShortcutAddedToStartMenu" {$tempvar|Add-Member -MemberType NoteProperty -Name "ShortcutAddedToStartMenu" -Value $t.Value}
-                "StartMenuFolder" {$tempvar|Add-Member -MemberType NoteProperty -Name "StartMenuFolder" -Value $t.Value}
-                "UserFilterEnabled" {$tempvar|Add-Member -MemberType NoteProperty -Name "UserFilterEnabled" -Value $t.Value}
-                "Visible" {$tempvar|Add-Member -MemberType NoteProperty -Name "Visible" -Value $t.Value}
-                "WaitForPrinterCreation" {$tempvar|Add-Member -MemberType NoteProperty -Name "WaitForPrinterCreation" -Value $t.Value}
-                "WorkingDirectory" {$tempvar|Add-Member -MemberType NoteProperty -Name "WorkingDirectory" -Value $t.Value}
-            }
-         }
-    }
-
-return $tempvar
-}
-
 function clean-FTAobject ($FTA)
 {
-$tempvar = @{}
-foreach($t in $app.PSObject.Properties)
+$tempvarfta = New-Object PSCustomObject
+foreach($t in $fta.PSObject.Properties)
     {       
         if(-not ([string]::IsNullOrWhiteSpace($t.Value)))
         {
             switch ($t.name)
             {
-                "ExtensionName" {$tempvar|Add-Member -MemberType NoteProperty -Name "ExtensionName" -Value $t.Value}
-                "ContentType" {$tempvar|Add-Member -MemberType NoteProperty -Name "ContentType" -Value $t.Value}
-                "ApplicationType" {$tempvar|Add-Member -MemberType NoteProperty -Name "ApplicationType" -Value $t.Value}
-                "HandlerOpenArguments" {$tempvar|Add-Member -MemberType NoteProperty -Name "HandlerOpenArguments" -Value $t.Value}
-                "HandlerDescription" {$tempvar|Add-Member -MemberType NoteProperty -Name "HandlerDescription" -Value $t.Value}
-                "HandlerName" {$tempvar|Add-Member -MemberType NoteProperty -Name "HandlerName" -Value $t.Value}
+                "ExtensionName" {$tempvarfta|Add-Member -MemberType NoteProperty -Name "ExtensionName" -Value $t.Value}
+                "ContentType" {$tempvarfta|Add-Member -MemberType NoteProperty -Name "ContentType" -Value $t.Value}
+                "HandlerOpenArguments" {$tempvarfta|Add-Member -MemberType NoteProperty -Name "HandlerOpenArguments" -Value $t.Value}
+                "HandlerDescription" {$tempvarfta|Add-Member -MemberType NoteProperty -Name "HandlerDescription" -Value $t.Value}
+                "HandlerName" {$tempvarfta|Add-Member -MemberType NoteProperty -Name "HandlerName" -Value $t.Value}
             }
          }
     }
 
-return $tempvar
+return $tempvarfta
 }
 
 if ($XDEXPORT)
@@ -225,6 +184,14 @@ $dgmatch = Get-BrokerDesktopGroup -AdminAddress $xdhost -Name $dg.DGNAME -ErrorA
                 $icon = New-BrokerIcon -AdminAddress $xdhost -EncodedIconData $app.EncodedIconData
                 $newapp|Set-BrokerApplication -AdminAddress $xdhost -IconUid $icon.Uid
                 set-UserPerms $makeapp
+                if($app.FTA)
+                {
+                write-host HERE -ForegroundColor Yellow
+                    foreach ($fta in $app.FTA)
+                    {
+                    clean-FTAobject $fta|New-BrokerConfiguredFTA -ApplicationUid $newapp.Uid
+                    }
+                }
                 }
             }
         }
