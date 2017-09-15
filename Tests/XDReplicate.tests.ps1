b7bc1e0adef5d14c55993a119a162348fb8cdfad
Ryan Butler <butlerrc30@gmail.com>
2017-09-14 22:51:36 -0500

    module test

--- Tests/XDReplicate.tests.ps1
+++ Tests/XDReplicate.tests.ps1
@@ -2,6 +2,6 @@ $sut = "$env:APPVEYOR_BUILD_FOLDER\XDReplicate\XDReplicate.psd1"
 
 Describe 'Script Metadata Validation' {      
         it 'Script fileinfo should be ok' {
-            {Test-ScriptFileInfo $sut} | Should Not Throw
+            {Test-ModuleManifest $sut} | Should Not Throw
         }    
 }
