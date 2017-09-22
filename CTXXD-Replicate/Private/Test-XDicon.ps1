62d14ca701aa1d94aa06cc85a1af9fe465fd4bb7
Ryan Butler <butlerrc30@gmail.com>
2017-09-22 11:41:32 -0500

    adding icon back

---
+++ CTXXD-Replicate/Private/Test-XDicon.ps1
@@ -0,0 +1,38 @@
+function Test-XDicon
+{
+<#
+.SYNOPSIS
+    Tests to see if Icon exists and matches new application
+.DESCRIPTION
+    Tests to see if Icon exists and matches new application
+.PARAMETER APP
+    Newly created application
+.PARAMETER APPMATCH
+    Existing application
+.PARAMETER XDHOST
+    XenDesktop DDC hostname to connect to
+
+#>
+[CmdletBinding()]
+[OutputType([System.boolean])]
+Param (
+    [Parameter(Mandatory=$true)]$app, 
+    [Parameter(Mandatory=$true)]$appmatch, 
+    [Parameter(Mandatory=$true)][string]$xdhost
+    )
+
+    Write-Verbose "BEGIN: $($MyInvocation.MyCommand)"
+    $newicon = (Get-BrokerIcon -AdminAddress $xdhost -Uid ($appmatch.IconUid)).EncodedIconData
+    if($newicon -like $app.EncodedIconData)
+    {
+        Write-Verbose "Icons Match"
+        $match = $true
+    }
+    else
+    {
+        Write-Verbose "Icons do not match -ForegroundColor Yellow"
+        $match = $false
+    }
+return $match
+Write-Verbose "END: $($MyInvocation.MyCommand)"
+}
