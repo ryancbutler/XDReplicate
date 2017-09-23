e094175d6f2da60b225b251b0c36a3941b55b090
Ryan Butler <butlerrc30@gmail.com>
2017-09-22 20:44:18 -0500

    adding test to look at function name

--- Tests/Main.tests.ps1
+++ Tests/Main.tests.ps1
@@ -18,7 +18,7 @@ Describe "General project validation" {
     }
 
     $scriptAnalyzerRules = Get-ScriptAnalyzerRule
-    It "Checking formatting <file> " -TestCases $testCase {
+    It "<file> should pass ScriptAnalyzer" -TestCases $testCase {
         param($file)
         $analysis = Invoke-ScriptAnalyzer -Path  $file.fullname -Severity @('Warning','Error')   
         
@@ -48,12 +48,13 @@ Describe "Function validation" {
             $test.Count | Should Be 1
         }
 
-        It "<file> should match function name" -TestCases $testCase {
+        It "<file> should match function name and contain -XD" -TestCases $testCase {
             param($file)   
             $file.fullname | Should Exist
             $contents = Get-Content -Path $file.fullname -ErrorAction Stop
             $describes = [Management.Automation.Language.Parser]::ParseInput($contents, [ref]$null, [ref]$null)
             $test = $describes.FindAll({$args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst]}, $true) 
             $test[0].name | Should Be $file.basename
+            $test[0].name | Should BeLike "*-XD*"
         }
 }
